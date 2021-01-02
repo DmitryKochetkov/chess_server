8a4e92c674fe59327856d1283ec79f43fa3b3d19
Dmitry Kochetkov <dimitri.kochetkov@gmail.com>
2021-01-02 20:59:21 +0300

    UserControllerTests

--- src/main/java/com/chess_sfml/chess/controller/UserController.java
+++ src/main/java/com/chess_sfml/chess/controller/UserController.java
@@ -2,24 +2,25 @@ package com.chess_sfml.chess.controller;
 
 import com.chess_sfml.chess.dto.UserDto;
 import com.chess_sfml.chess.entity.User;
-import com.chess_sfml.chess.exception.NotFoundException;
 import com.chess_sfml.chess.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
     @Autowired
-    UserService userService;
+    private UserService userService;
 
     @GetMapping("/{userId}")
-    public ResponseEntity<UserDto> getUser(@PathVariable Long userId) {
-        User user = userService.findById(userId).orElseThrow();
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long userId) {
+        User user = userService.findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
         return ResponseEntity.ok(new UserDto(user));
     }
 }
---
+++ src/test/java/com/chess_sfml/chess/controller/UserControllerTests.java
@@ -0,0 +1,51 @@
+package com.chess_sfml.chess.controller;
+
+import com.chess_sfml.chess.entity.User;
+import com.chess_sfml.chess.service.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+@AutoConfigureMockMvc
+public class UserControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserService userServiceMock;
+
+    @Before
+    public void setup() {
+        User user = new User();
+        user.setUsername("best_user");
+        user.setFirstName("Ivan");
+        user.setLastName("Ivanov");
+        user.setPassword("i_dont_use_hashing");
+        when(userServiceMock.findById(1L)).thenReturn(java.util.Optional.of(user));
+    }
+
+    @Test
+    public void getUserByExistingId() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/user/{userId}", 1L))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getUserByNotExistingId() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/user/{userId}", 2L))
+                .andExpect(status().isNotFound());
+    }
+}
